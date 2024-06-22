ae5c4ab30ddb5462505c29e34c3e965d90971780
j-swaroop <jyothiswaroop654@gmail.com>
2024-06-22 13:48:52 +0530

    last

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json())
 const users = require('./routes/users')
 app.use('/users', users)
 
-
+let port = process.env.PORT || 3001
 try{
-    app.listen(3001, () => {
+    app.listen(port, () => {
         console.log('Server Running on port 3001');
     })
 }catch(e){
